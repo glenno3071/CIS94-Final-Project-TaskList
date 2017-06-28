7ea06d40ce4d723efc458cf10d3347204c84651d
glenno3071 <glenno3071@gmail.com>
2017-06-28 05:57:41 -0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,9 +5,10 @@ $page = new Page();
 $page->title = 'Delete';
 include 'header.php';
 
-<input type="submit" name="delete", value="confirm delete csv file 'tasklist'"><br>,br>;
+<input type="submit" name="delete", value="confirm delete csv file 'tasklist'"><br><br>;
 if(isset($_POST['delete']){
   unlink('tasklist.csv');
   echo "Deleted the csv file tasklist';
   }
+  
 include 'footer.php';
