04ea2277433f8dd6f4dad9eac85f2cb740d6e5ff
glenno3071 <glenno3071@gmail.com>
2017-06-28 06:09:03 -0700

    Update add.php

--- add.php
+++ add.php
@@ -14,13 +14,43 @@ include 'task.php';
 // files above
 
 function addTask($input) {
-     // 2.
+  if(isset($_POST['submit'])){
+    //collect form data
+    $task = $_POST['task'];
+    $date = $_POST['date'];
+    $desc = $_POST['desc'];
+    $datec = $_POST['datec'];
+    //check if task and date is set is set
+    if($task ==''){
+        $error[] = 'task is required';
+    }
+    if($date ==''){
+        $error[] = 'date is required';
+    }
+  
+// if there are no errors
+ if(!isset($error)){
+     
+// Creates a new csv file     
+$filename=fopen("tasklist.csv","a");
+fwrite($filename,"$task,$date,$desc,$datec\n");
+print "task added";
+fclose($filename);
+ }
+}
+//if their are errors display them
+if(isset($error)){
+    foreach($error as $error){
+        echo "<p style='color:#ff0000'>$error</p>";
+    }
+ }
+ // 2.
      // Put code here to add the task to the CSV file
     // you should use the Task class that you developed earlier here
     // the following function is only called right now
     // to "debug" the addTask function, it should not be in the final
     // version of this code
-    var_dump($input);
+ //   var_dump($input);
     
 }
 
